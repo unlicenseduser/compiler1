fa63be23108894306e98ab1114902efdd0a68ecf
unlicenseduser <yevgenyv@mailstud.hit.ac.il>
2018-03-20 02:47:33 +0200

    Add files via upload

---
+++ scanner.h
@@ -0,0 +1,19 @@
+#define UNARY_tok 1
+#define ID_tok  2
+#define INT_NUM_tok 3
+#define REAL_NUM_tok 4
+#define AR_OP_tok 5
+#define KEYWORDS_tok 6
+#define SEPARATION_SIGNS_tok 7
+#define ASSIGNMENT_tok 8
+#define POINTER_tok 9
+#define COMPARISON_tok 10
+
+
+
+
+
+
+
+
+
